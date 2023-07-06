9471f1ceab4c5b878dae13d88f6a1523036796c5
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2023-07-06 18:44:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project     = "bionic-baton-384504"
+  region      = "us-central1"
+}
+
 resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
