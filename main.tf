a15d43df87a20b75ff205afb399273e02203e03a
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2023-07-06 18:40:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
+}
+  
 
   boot_disk {
     initialize_params {
@@ -26,4 +26,4 @@ resource "google_compute_instance" "default" {
       // Ephemeral public IP
     }
   }
-}
+
