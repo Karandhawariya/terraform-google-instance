91f78f0f66738bdf8e72d633c35c7a561426205a
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2023-10-02 20:54:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
 
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  project      = "mythical-zodiac-399615"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
